8bf863c5ac0499b27209d9053fedc70998c16ca3
dani3lm3na1999 <dani3lm3na@gmail.com>
2022-07-01 18:39:51 -0600

    Change enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  apiURL:'https://honeyappservice.azurewebsites.net/api'
+  apiURL:'https://honeyappservice.azurewebsites.net/api',
+  firebaseConfig: {
+    apiKey: "AIzaSyDmZpkS0TQ7zVYHlkwQRIESFkSBarKbxys",  
+    authDomain: "honeyapp-da6ed.firebaseapp.com",  
+    projectId: "honeyapp-da6ed",  
+    storageBucket: "honeyapp-da6ed.appspot.com",  
+    messagingSenderId: "9369192103",  
+    appId: "1:9369192103:web:c35d8a248769030d927d82",  
+    measurementId: "G-FR8WLVEFT6"  
+  } 
 };
